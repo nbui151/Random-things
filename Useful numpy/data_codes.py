1217b2bef7e9a6c6c9735b0c8f365127ec69eaf6
Nhung Bui <nhungbui151@gmail.com>
2017-09-27 11:55:08 -0700

    Update data_codes.py

--- Useful numpy/data_codes.py
+++ Useful numpy/data_codes.py
@@ -1,3 +1,9 @@
 import matplotlib.pyplot as plot
 plot.hist(y, 100) # plot histogram 
 
+# correlation matrix 
+import matplotlib.pyplot as plt
+import seaborn as sns
+corrmat = data.corr() 
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=0.8, square=True);
